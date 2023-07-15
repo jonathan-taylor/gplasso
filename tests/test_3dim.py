7ae456409493f1672015801715d9f7a60bfc3df6
Jonathan Taylor <jonathan.taylor@stanford.edu>
2023-07-15 14:59:49 -0700

    a 3d test case

--- tests/test_3dim.py
+++ tests/test_3dim.py
@@ -3,72 +3,121 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
-from kernel_calcs import covariance_structure
-from peaks import extract_peaks, extract_points
-from taylor_expansion import taylor_expansion_window
-from randomized_inference import setup_inference, inference
 
-from gplasso import fit_gp_lasso
+from gplasso.api import (covariance_structure,
+                         default_clusters,
+                         GridLASSOInference,
+                         SVDSampler)
 
-def test_3dim(seed=10, svd_info=None):
+from joblib import Memory
+location = './cachedir'
+memory = Memory(location, verbose=0)
+
+def compute_svd_info(xval,
+                     yval,
+                     zval,
+                     precision,
+                     var=1):
+
+    grid = np.meshgrid(xval, yval, zval, indexing='ij')
+    K = covariance_structure.gaussian(precision=precision,
+                                      grid=grid,
+                                      var=var)
+    S_ = K.C00(None, None)
+    npt = int(np.sqrt(np.product(S_.shape)))
+    shape = S_.shape[:len(S_.shape)//2]
+    S_ = S_.reshape(npt, npt)
+    U, D = np.linalg.svd(S_)[:2]
+
+    return U, D, npt, shape
+
+compute_svd_info = memory.cache(compute_svd_info)
+
+
+def instance(seed=10,
+             svd_info=None,
+             plot=False):
 
     if seed is not None:
         rng = np.random.default_rng(seed)
     else:
         rng = np.random.default_rng()
         
-    nx, ny, nz = 31, 21, 11
+    nx, ny, nz = 11, 12, 13
     
     xval = np.linspace(-5,5,nx)
     yval = np.linspace(-3,8,ny)
-    zval = np.linspace(-2,0,nz)
+    zval = np.linspace(0,2,nz)
+
     grid = np.meshgrid(xval, yval, zval, indexing='ij')
 
-    precision = np.diag([1.4, 1.3, 1.1])
+    precision = np.diag([1.4, 2.1, 1.1])
+
+    svd_info = compute_svd_info(xval,
+                                yval,
+                                zval,
+                                precision)
+    
+    K_sampler = SVDSampler(*svd_info)
     K = covariance_structure.gaussian(precision=precision,
-                                      grid=grid)
+                                      grid=grid,
+                                      sampler=K_sampler)
 
-    if svd_info is None:
-        S_ = K.C00(None, None).reshape(nx*ny*nz, nx*ny*nz)
-        A, D = np.linalg.svd(S_)[:2]
-        svd_info = A, D
-    else:
-        A, D = svd_info
-
-    Z = A @ (np.sqrt(D) * rng.standard_normal(nx*ny*nz))
-    Z = Z.reshape((nx, ny, nz))
+    Z = K.sample(rng=rng)
 
     proportion = 0.8
     var_random = (1 - proportion) / proportion
+    svd_info_rand = compute_svd_info(xval,
+                                     yval,
+                                     zval,
+                                     precision,
+                                     var=var_random)
+    omega_sampler = SVDSampler(*svd_info_rand)
+    
     K_omega = covariance_structure.gaussian(precision=precision,
-                                            grid=grid, var=var_random)
-    omega = A @ (np.sqrt(D) * rng.standard_normal(nx*ny*nz) * np.sqrt(var_random))
-    omega = omega.reshape((nx, ny, nz))
-    
+                                            grid=grid,
+                                            var=var_random,
+                                            sampler=omega_sampler)
+
     penalty_weights = 2 * np.sqrt(1 + var_random) * np.ones_like(Z)
-    E, soln, subgrad = fit_gp_lasso(Z + omega,
-                                    [K, K_omega],
-                                    penalty_weights)
+
+    lasso = GridLASSOInference((xval, yval, zval),
+                               penalty_weights,
+                               K,
+                               K_omega,
+                               inference_kernel=None)
+
+    E, soln, subgrad = lasso.fit(Z,
+                                 rng=rng)
+
+    # this is 3d grid specific
     
-    signs = np.sign(subgrad[E])
+    cluster_df = default_clusters(E,
+                                  K,
+                                  cor_threshold=0.9)
+    selection = []
 
-    second_order = taylor_expansion_window((xval, yval, zval),
-                                           [Z, omega],
-                                           np.nonzero(E))
+    for label in np.unique(cluster_df['Cluster']):
+        cur_df = cluster_df[lambda df: cluster_df['Cluster'] == label]
+        selection.append(cur_df.iloc[rng.choice(cur_df.shape[0], 1)])
+    selection = pd.concat(selection)
 
-    tangent_bases = [np.identity(3) for _ in range(len(E))]
-    normal_info = [(np.zeros((0, 3)), np.zeros((0, 0))) for _ in range(len(E))]
+    model_spec = pd.DataFrame({'Value':[True] * len(selection['Index']),
+                               'Displacement':[False] * len(selection['Index'])},
+                              index=selection['Index'])
+    model_spec.loc[[selection['Index'][0]],'Value'] = False
 
+    mid = (xval.shape[0]//2, yval.shape[0]//2, zval.shape[0]//2)
+    extra_pt = pd.DataFrame({'Value':[True],
+                             'Displacement':[False],
+                             'Index':[mid]}).set_index('Index')
+
+    model_spec = pd.concat([model_spec, extra_pt])
+    
+    model_spec = lasso.extract_peaks(selection['Index'],
+                                     model_spec=model_spec)
+    
     E_nz = np.nonzero(E)
-
-    peaks, clusters, _ = extract_peaks(E_nz,
-                                       second_order,
-                                       tangent_bases,
-                                       normal_info,
-                                       K,
-                                       signs,
-                                       penalty_weights,
-                                       seed=1)
 
     inactive = np.ones(soln.shape, bool)
     for i, j, k in zip(*E_nz):
@@ -76,28 +125,25 @@ def test_3dim(seed=10, svd_info=None):
                  max(j-2, 0):(j+2),
                  max(k-2, 0):(k+2)] = 0
 
-    info = setup_inference(peaks,
-                           inactive,
-                           subgrad,
-                           penalty_weights,
-                           K,
-                           K_omega,
-                           inference_kernel=None,
-                           displacement=True)
+    param_default = lasso.setup_inference(inactive)
 
-    pivot_carve, disp_carve = inference(info,
-                                        one_sided=False,
-                                        param=None,
-                                        level=0.9)
+    pivot_carve, disp_carve = lasso.summary(one_sided=False,
+                                            param=param_default,
+                                            level=0.9)
 
-    return pivot_carve, svd_info
+    return pivot_carve
+
+def test_3d():
+
+    instance(seed=7)
+    
 
 if __name__ == '__main__':
     dfs = []
-    svd_info = None
+    
     for _ in range(200):
         try:
-            df, svd_info = test_3dim(seed=None, svd_info=svd_info)
+            df = instance(seed=None)
             dfs.append(df)
         except KeyboardInterrupt:
             break
